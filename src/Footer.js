3bdd04e6bf144f125321545ae06f20a7f925ec71
Olabode Adewumi <57285170+Boldmax@users.noreply.github.com>
2021-02-25 22:16:49 +0100

    Updated

--- src/Footer.js
+++ src/Footer.js
@@ -18,13 +18,13 @@ export default function Footer() {
                         <a href="#portfolio">Portfolio</a>
                         <a href="#contact">Contact</a>
                     </div>
-                    <div className="copyright">
-                        <div className="logos">
-                            <a href="https://twitter.com/Bold_max1?s=09" className="bi bi-twitter">Twitter</a>
-                            <a href="https://www.linkedin.com/mwlite/in/adewumi-olabode-olalekan-091a9171" className="bi bi-linkedin">LinkedIn</a>
-                            <a href="https://github.com/Boldmax" className="bi bi-github">GitHub</a>
-                        </div>
-                        <p>Copyright © 2021 boldmax</p>
+                    <div className="copyright">                       
+                         <ul className="logos">
+                           <li> <a href="https://twitter.com/Bold_max1?s=09"><i className="bi bi-twitter"></i></a></li>
+                           <li><a href="https://www.linkedin.com/mwlite/in/adewumi-olabode-olalekan-091a9171"><i className="bi bi-linkedin"></i></a></li>
+                           <li><a href="https://github.com/Boldmax"><i className="bi bi-github"></i></a></li>
+                        </ul>
+                        <p>Copyright">&copy; 2021 boldmax</p>
                     </div>
                 </div>
             </div>
