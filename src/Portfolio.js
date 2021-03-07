c0b7cca4d52da8dbf0de83b74fe4fe9864017044
Boldmax <maxbold0007@gmail.com>
2021-03-07 14:42:09 +0100

    new update

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -18,8 +18,8 @@ export default function Portfolio() {
             <div className="portfolio-posit">
                 <div className="portfo-card">
                     <Animate scaleX={hovered ? 1.3 : 1} scaleY={hovered ? 1.3 : 1}>
-                        <Card onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} data-aos="fade-up" style={{ width: '20rem', height: '14rem', borderRadius: "0.7rem", padding: "0.7rem", border: "0.2rem solid #00a2ff", marginBottom: "1.7rem" }} className="bg-light text-white">
-                            <Card.Img style={{ borderRadius: "0.7rem", border: "0.03rem solid black" }} src={eazyRent} alt="Card image" />
+                        <Card onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} data-aos="fade-up" style={{ width: '20rem', height: '14rem', borderRadius: "0.7rem", padding: "0.7rem", marginBottom: "1.7rem" }} className="bg-light text-white">
+                            <Card.Img style={{ borderRadius: "0.7rem" }} src={eazyRent} alt="Card image" />
                             <Card.ImgOverlay>
                                 <h2 className="m-title">EazyRent</h2>
                                 <Card.Text className="sub-title">EazyRent user interface(frontend) was built with HTML, CSS and Javascript technology while the backend was built with python technology. </Card.Text>
@@ -29,8 +29,8 @@ export default function Portfolio() {
                             </Card.ImgOverlay>
                         </Card>
                     </Animate>
-                    <Card data-aos="fade-down" style={{ width: '20rem', height: '14rem', borderRadius: "0.7rem", padding: "0.7rem", border: "0.2rem solid #00a2ff", marginBottom: "1.7rem" }} className="bg-light text-white">
-                        <Card.Img style={{ borderRadius: "0.7rem", border: "0.03rem solid black" }} src={backGroun} alt="Card image" />
+                    <Card data-aos="fade-up" style={{ width: '20rem', height: '14rem', borderRadius: "0.7rem", padding: "0.7rem",  marginBottom: "1.7rem" }} className="bg-light text-white">
+                        <Card.Img style={{ borderRadius: "0.7rem"}} src={backGroun} alt="Card image" />
                         <Card.ImgOverlay>
                             <h2 className="m-title">Card Title</h2>
                             <Card.Text className="sub-title">Lorem, ipsum consectetur adipisicing elit. Nostrum aliquid maiores,repudiandae qui accusamus temporibus! Saepe ab illo cum?</Card.Text>
@@ -39,8 +39,8 @@ export default function Portfolio() {
                             </Card.Body>
                         </Card.ImgOverlay>
                     </Card>
-                    <Card data-aos="fade-up" style={{ width: '20rem', height: '14rem', borderRadius: "0.7rem", padding: "0.7rem", border: "0.2rem solid #00a2ff", marginBottom: "1.7rem" }} className="bg-light text-white">
-                        <Card.Img style={{ borderRadius: "0.7rem", border: "0.03rem solid black" }} src={backGroun} alt="Card image" />
+                    <Card data-aos="fade-up" style={{ width: '20rem', height: '14rem', borderRadius: "0.7rem", padding: "0.7rem", marginBottom: "1.7rem" }} className="bg-light text-white">
+                        <Card.Img style={{ borderRadius: "0.7rem" }} src={backGroun} alt="Card image" />
                         <Card.ImgOverlay>
                             <h2 className="m-title">Card Title</h2>
 
@@ -50,8 +50,8 @@ export default function Portfolio() {
                             </Card.Body>
                         </Card.ImgOverlay>
                     </Card>
-                    <Card data-aos="fade-down" style={{ width: '20rem', height: '14rem', borderRadius: "0.7rem", padding: "0.7rem", border: "0.2rem solid #00a2ff", marginBottom: "1.7rem" }} className="bg-light text-white">
-                        <Card.Img style={{ borderRadius: "0.7rem", border: "0.03rem solid black" }} src={backGroun} alt="Card image" />
+                    <Card data-aos="fade-up" style={{ width: '20rem', height: '14rem', borderRadius: "0.7rem", padding: "0.7rem", marginBottom: "1.7rem" }} className="bg-light text-white">
+                        <Card.Img style={{ borderRadius: "0.7rem" }} src={backGroun} alt="Card image" />
                         <Card.ImgOverlay>
                             <h2 className="m-title">Card Title</h2>
 
