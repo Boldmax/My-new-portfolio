caa3416be4c6ed57a7d12986e7cfbe3f77adec55
Boldmax <olabode.adewunmi@mkobo.com.ng>
2021-12-19 22:44:38 +0100

    Completed

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,66 +1,67 @@
 import React, { useEffect } from 'react';
-import { Card } from "react-bootstrap";
-import backGroun from "./backgrd (2).jpg";
-import eazyRent from "./EaZyRent.png";
+/* import { div } from "react-bootstrap"; */
+/* import eazyRent from "./EaZyRent.png"; */
 import Aos from "aos";
 import "aos/dist/aos.css";
-import { Animate } from "react-rebound";
+import portfolio1 from "./images/portfolio1.png";
+import portfolio2 from "./images/portfolio2.png";
+import portfolio3 from "./images/portfolio3.png";
+import portfolio4 from "./images/portfolio4.png";
+//import { Animate } from "react-rebound";
 
 export default function Portfolio() {
-    const [hovered, setHovered] = React.useState(false);
+    /* const [hovered, setHovered] = useState(false); */
 
     useEffect(() => {
-        Aos.init({ duration: 2000 });
+        Aos.init({ duration: 1000 });
     }, []);
     return (
-        <div className="portfolio-card" id="portfolio">
+        <div className="portfolio" id="portfolio">
             <h3>PORTFOLIO</h3>
             <div className="portfolio-posit">
-                <div className="portfo-card">
-                    <Animate scaleX={hovered ? 1.3 : 1} scaleY={hovered ? 1.3 : 1}>
-                        <Card onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} data-aos="fade-up" style={{ width: '20rem', height: '14rem', borderRadius: "0.7rem", padding: "0.7rem", marginBottom: "1.7rem" }} className="bg-light text-white">
-                            <Card.Img style={{ borderRadius: "0.7rem" }} src={eazyRent} alt="Card image" />
-                            <Card.ImgOverlay>
-                                <h2 className="m-title">EazyRent</h2>
-                                <Card.Text className="sub-title">EazyRent user interface(frontend) was built with HTML, CSS and Javascript technology while the backend was built with python technology. </Card.Text>
-                                <Card.Body>
-                                    <Card.Link href="#">Another Link</Card.Link>
-                                </Card.Body>
-                            </Card.ImgOverlay>
-                        </Card>
-                    </Animate>
-                    <Card data-aos="fade-up" style={{ width: '20rem', height: '14rem', borderRadius: "0.7rem", padding: "0.7rem",  marginBottom: "1.7rem" }} className="bg-light text-white">
-                        <Card.Img style={{ borderRadius: "0.7rem"}} src={backGroun} alt="Card image" />
-                        <Card.ImgOverlay>
-                            <h2 className="m-title">Card Title</h2>
-                            <Card.Text className="sub-title">Lorem, ipsum consectetur adipisicing elit. Nostrum aliquid maiores,repudiandae qui accusamus temporibus! Saepe ab illo cum?</Card.Text>
-                            <Card.Body>
-                                <Card.Link href="#">Another Link</Card.Link>
-                            </Card.Body>
-                        </Card.ImgOverlay>
-                    </Card>
-                    <Card data-aos="fade-up" style={{ width: '20rem', height: '14rem', borderRadius: "0.7rem", padding: "0.7rem", marginBottom: "1.7rem" }} className="bg-light text-white">
-                        <Card.Img style={{ borderRadius: "0.7rem" }} src={backGroun} alt="Card image" />
-                        <Card.ImgOverlay>
-                            <h2 className="m-title">Card Title</h2>
+                <div className="portfo-cont">
+                    {/* <Animate scaleX={hovered ? 1.3 : 1} scaleY={hovered ? 1.3 : 1}> */}
+                    <div className="portfo-card" data-aos="fade-up" >
+                        <img src={portfolio1} alt="portfolio" />
+                        <div>
+                            <h4>Mobile Restaurant</h4>
+                            <p>Developed with React, Redux, Node and Express Js. Website
+                                adaptable to all devices, with ui components and animated interactions. </p>
+                            <button href="https://stark-brook-46223.herokuapp.com/">View App</button>
+                        </div>
+                    </div>
+                    {/* </Animate> */}
 
-                            <Card.Text className="sub-title">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum aliquid maiores, id fugiat, quod! Saepe ab illo cum?</Card.Text>
-                            <Card.Body>
-                                <Card.Link href="#">Another Link</Card.Link>
-                            </Card.Body>
-                        </Card.ImgOverlay>
-                    </Card>
-                    <Card data-aos="fade-up" style={{ width: '20rem', height: '14rem', borderRadius: "0.7rem", padding: "0.7rem", marginBottom: "1.7rem" }} className="bg-light text-white">
-                        <Card.Img style={{ borderRadius: "0.7rem" }} src={backGroun} alt="Card image" />
-                        <Card.ImgOverlay>
-                            <h2 className="m-title">Card Title</h2>
+                    <div data-aos="fade-up" className="portfo-card">
+                        <img src={portfolio2} alt="portfolio" />
+                        <div>
+                            <h4>Goodies Shop</h4>
+                            <p>Developed with React, Redux, Node and Express Js. Website
+                                adaptable to all devices, with ui components and animated interactions.</p>
+                            <button href="https://classicbuy-ecommerce.herokuapp.com/">View App</button>
+                        </div>
+                    </div>
 
-                            <Card.Text className="sub-title">Lorem, ipsum dolor sit amet . Nostrum aliquid maiores, id fugiat, quod, culpa laborum repellendus perspiciatis </Card.Text>
-                            <Card.Body>
-                                <Card.Link href="#">Another Link</Card.Link>
-                            </Card.Body>
-                        </Card.ImgOverlay>
-                    </Card>
+                    <div data-aos="fade-up" className="portfo-card">
+                        <img src={portfolio3} alt="portfolio" />
+                        <div>
+                            <h4>Trendy Shoes</h4>
+                            <p>Developed with React, Redux, Node and Express Js. Website
+                                adaptable to all devices, with ui components and animated interactions.</p>
+                            <button href="https://trendyshoeapp.herokuapp.com/">View App</button>
+                        </div>
+                    </div>
+
+                    <div data-aos="fade-up" className="portfo-card">
+                        <img src={portfolio4} alt="portfolio" />
+                        <div>
+                            <h4>Live Love Foundation</h4>
+                            <p>Developed with React, Redux, Node and Express Js. Website
+                                adaptable to all devices,
+                                with ui components and animated interactions. </p>
+                            <button href="https://livelovefoundatn.netlify.app/">View App</button>
+                        </div>
+                    </div>
 
                 </div>
             </div>
