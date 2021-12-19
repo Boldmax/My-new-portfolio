3e77eec7c3323b71f88850e6351e3876f4b29984
Boldmax <olabode.adewunmi@mkobo.com.ng>
2021-12-19 14:37:46 +0100

    Completed

--- src/MySkills.js
+++ src/MySkills.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { card } from "react-bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
