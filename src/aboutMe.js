475cc6e88d7e5bb71cdae9838e146c15c4930f9a
Boldmax <olabode.adewunmi@mkobo.com.ng>
2021-12-20 22:04:53 +0100

    Completed

--- src/aboutMe.js
+++ src/aboutMe.js
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
 import image from "./mne.jpg";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function AboutMe() {
+
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+    }, []);
+
     return (
-        <div className="about-wrapper" id="about-me">
+        <div className="about-wrapper" id="about-me" data-aos="fade-left">
             <div className="card1">
                 <img src={image} alt="my pictures" />
             </div>
